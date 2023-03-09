17c163acabc4919f298e71885c976ae938b275f3
Esteban Garcia <garcia00esteban@gmail.com>
2023-03-09 18:29:09 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Terraform
 Terraform scripts
 
-These are some of my terraform scripts to better understand and work with Terraform in an AWS environment.  
+These are some of my terraform scripts to better understand and work with Terraform in an AWS environment.  Committed to being a better devops engineer. 
--- excercise1/my_instance.tf
+++ excercise1/my_instance.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "intro" {
-  ami                    = "ami-006dcf34c09e50022"
+  ami                    = ""
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
-  key_name               = "dove-key"
-  vpc_security_group_ids = ["sg-0af3cbd336eaf7d2d"]
+  key_name               = ""
+  vpc_security_group_ids = [""]
   tags = {
     Name    = "Dove-Instance"
     Project = "Dove"
