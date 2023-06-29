98be5141cebac085c128616c81b818760536b1c2
hashtagesteban <114188090+hashtagesteban@users.noreply.github.com>
2023-06-29 17:39:44 +0000

    added second excercis. Created vars, providers, and instance

---
+++ excercise 2/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "dov-inst" {
+    ami = var.AMIS[var.region]
+    instance_type = "t2.micro"
+    availability_zone = var.ZONE1
+    key_name = "dove-key"
+ vpc_security_group_ids = []
+ tags = {
+    Name    = "Dove-Instance"
+
+
+}
