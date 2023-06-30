3ef7d6eafed5204c02dcafdeb5a320e82bb410b8
hashtagesteban <114188090+hashtagesteban@users.noreply.github.com>
2023-06-30 18:28:29 +0000

    added excercise 3: provisioners

---
+++ excercise 3/vars.tf
@@ -0,0 +1,18 @@
+variable "REGION" {
+    default = "us-east-2"
+}
+
+variable "ZONE1" {
+    default = "us-east-2a"
+}
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-2 = "enter AMI"
+        us-east-1 = "enter AMI"
+    }  
+
+variable "USER" {
+    default = "ec2-user"
+}
